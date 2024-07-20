86b31cead4e588d27f2f49a44356394a48850d38
Abhijeet Singh <abhijeet.singh1462@gmail.com>
2024-07-20 16:50:54 +0530

    Made changes to utils.ts so that they can perform CRUD with state

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -17,9 +17,11 @@ type TaskObj = {
 
 // Utilities Functions fot Task
 
-export function createTask(target: TaskObj[], title: string, notes: string | null, listId: string): void {
+export function createTask(target: TaskObj[], set: (a: TaskObj[]) => void, title: string, notes: string | null, listId: string): void {
+    const data: TaskObj[] = target
     const element: TaskObj = { title: title, notes: notes, id: uuidv4(), listId: listId, stared: false, completed: false }
-    target.unshift(element)
+    data.unshift(element)
+    set(data)
 }
 
 export function taskObject(target: TaskObj[], id: string): TaskListObj {
@@ -27,8 +29,10 @@ export function taskObject(target: TaskObj[], id: string): TaskListObj {
     return element
 }
 
-export function updatedTask(target: TaskObj[], id: string, property: string, value: string): void {
-    const element: TaskObj = target.filter(obj => obj.id == id)[0]
+export function updatedTask(target: TaskObj[], set: (a: TaskObj[]) => void, id: string, property: string, value: string): void {
+    const data: TaskObj[] = target
+    const index: number = data.findIndex(obj => obj.id === id)
+    const element: TaskObj = data[index]
     property == 'title' && (element.title = value)
     property == 'notes' && (element.notes = value)
     property == 'listId' && (element.listId = value)
@@ -50,26 +54,31 @@ export function updatedTask(target: TaskObj[], id: string, property: string, val
             console.log('Invalid Value')
         }
     }
+    set(data)
 }
 
-export function deleteTask(target: TaskObj[], array: string[]): void {
+export function deleteTask(target: TaskObj[], set: (a: TaskObj[]) => void, array: string[]): void {
+    const data: TaskObj[] = target
     array.forEach((id: string) => {
-        const index: number = target.findIndex((obj: TaskObj) => obj.id == id)
+        const index: number = data.findIndex((obj: TaskObj) => obj.id == id)
         if (index !== -1) {
             target.splice(index, 1)
         }
     })
+    set(data)
 }
 
 
 
 // Utilities Functions for TaskList 
 
-export function createTaskList(target: TaskListObj[], title: string): void {
-    target.push({
+export function createTaskList(target: TaskListObj[], set: (a: TaskListObj[]) => void, title: string): void {
+    const data: TaskListObj[] = target
+    data.push({
         id: uuidv4(),
         title: title
     })
+    set(data)
 }
 
 export function taskListObject(target: TaskListObj[], id: string): TaskListObj {
@@ -77,13 +86,17 @@ export function taskListObject(target: TaskListObj[], id: string): TaskListObj {
     return element
 }
 
-export function updateTaskList(target: TaskListObj[], id: string, title: string): void {
-    const element: TaskListObj = target.filter(obj => obj.id == id)[0]
+export function updateTaskList(target: TaskListObj[], set: (a: TaskListObj[]) => void, id: string, title: string): void {
+    const data: TaskListObj[] = target
+    const index: number = data.findIndex(obj => obj.id === id)
+    const element: TaskListObj = data[index]
     element.title = title
+    set(data)
 }
 
-export function deleteTaskList(target: TaskListObj[], tasks: TaskObj[], id: string): void {
-    const index: number = target.findIndex((obj: TaskListObj) => obj.id == id)
+export function deleteTaskList(target: TaskListObj[], setTaskList: (a: TaskListObj[]) => void, tasks: TaskObj[], setTask: (a: TaskObj[]) => void, id: string): void {
+    const data: TaskListObj[] = target
+    const index: number = data.findIndex((obj: TaskListObj) => obj.id == id)
     if (index !== -1) {
         const task_list: string[] = []
         tasks.forEach((obj: TaskObj) => {
@@ -91,8 +104,9 @@ export function deleteTaskList(target: TaskListObj[], tasks: TaskObj[], id: stri
                 task_list.push(obj.id)
             }
         })
-        target.splice(index, 1)
-        deleteTask(tasks, task_list)
+        data.splice(index, 1)
+        setTaskList(data)
+        deleteTask(tasks, setTask, task_list)
     }
 }
 
