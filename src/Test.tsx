e8597c331e837496940d48992d45b4c9dfe8762c
Abhijeet Singh <abhijeet.singh1462@gmail.com>
2024-07-27 15:26:43 +0530

    Deleted Test Page and Unified the CSS files

--- src/Test.tsx
+++
@@ -1,9 +0,0 @@
-import './Test.css'
-
-export default function Test() {
-    return (
-        <>
-            <h1 className='text-8xl font-poppins text-cWhite m-1'>Hello</h1>
-        </>
-    )
-}
