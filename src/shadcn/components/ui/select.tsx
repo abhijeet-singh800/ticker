208266f6d145cd27784b58951fa15217d993b91c
Abhijeet Singh <abhijeet.singh1462@gmail.com>
2024-09-04 03:14:58 +0530

    Fixed Inconsistant styling in the Select Schadcn Component

--- src/shadcn/components/ui/select.tsx
+++ src/shadcn/components/ui/select.tsx
@@ -17,7 +17,7 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      " h-10 w-full items-center text-center rounded-full bg-cWhite text-cBlack px-3 py-2 font-medium disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 font-poppins",
+      " h-10 w-full items-center text-center rounded-full bg-cWhite text-cBlack text-base font-medium px-3 py-2 font-medium disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 font-poppins",
       className
     )}
     {...props}
@@ -70,7 +70,7 @@ const SelectContent = React.forwardRef<
     <SelectPrimitive.Content
       ref={ref}
       className={cn(
-        "relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-3xl border bg-cWhite text-cBlack font-medium shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+        "relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-3xl border bg-cWhite text-cBlack text-base font-medium shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
         position === "popper" &&
         "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
         className
