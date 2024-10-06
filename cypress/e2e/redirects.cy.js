b1764940f5a554e889dcc2323d22a532620a5485
Abhijeet Singh <abhijeet.singh1462@gmail.com>
2024-10-06 12:42:38 +0530

    Improved Redirect Cypress Test with baseUrl

--- cypress/e2e/redirects.cy.js
+++ cypress/e2e/redirects.cy.js
@@ -33,7 +33,7 @@ describe('Testing the Redirects for the App', () => {
         cy.get('h2').should('contain', 'Page not found')
         cy.get('button').click()
 
-        cy.url().should('eq', 'http://localhost:5173/')
+        cy.url().should('eq', Cypress.config('baseUrl') + '/')
     })
 
     it('Testing Card Redirects', () => {
